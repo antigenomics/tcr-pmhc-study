b9628d2b4651f9f7091980466284d3560e3a863e
mikessh <mikhail.shugay@gmail.com>
2015-12-25 09:51:16 +0300

    Functional version

--- structure/src/structure.py
+++ structure/src/structure.py
@@ -1,38 +1,50 @@
 import argparse
+import warnings
+from shutil import rmtree
 
 import pandas as pd
 from Bio.PDB import *
 
 from util import *
 
-parser = argparse.ArgumentParser(description='structure.py annotation_table output_table')
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description="structure.py annotation_table output_table")
 
 args = parser.parse_args()
 
 # df = pd.read_table(args[0])
 
-table = pd.read_table('../../result/final.annotations.txt')  # TODO
+table = pd.read_table("../../result/final.annotations.txt")  # TODO
 
-bypdb = table.groupby('pdb_id')
+bypdb = table.groupby("pdb_id")
 
 # Create temporary folders
 (pdb_dir, gmx_dir) = '../tmp/pdb/', '../tmp/gmx/'
-for d in [pdb_dir, gmx_dir]:
-    if not os.path.exists(d):
-        os.makedirs(d)
-    else:
-        clear_folder(d)
+
+if os.path.exists(gmx_dir):
+    rmtree(gmx_dir)
+
+if not os.path.exists(pdb_dir):
+    os.makedirs(pdb_dir)
+
+os.makedirs(gmx_dir)
+
+pdb_list = PDBList()
+pdb_parser = PDBParser()
 
 # Main loop
-for pdb_id, pdb_group in bypdb:
-    pdb_file = '{}pdb{}.ent'.format(pdb_dir, pdb_id)
-    gmx_prefix = '{}{}'.format(gmx_dir, pdb_id)
+for pdb_id, pdb_group in itertools.islice(bypdb, 2, 4):  # TODO
+    # pdb_file = "{}pdb{}.ent".format(pdb_dir, pdb_id)
 
     # Load PDB file
-    PDBList().retrieve_pdb_file(pdb_id, pdir=pdb_dir)
+    pdb_file = pdb_list.retrieve_pdb_file(pdb_id, pdir=pdb_dir)
 
-    # Load model from fixed pdb file
-    model = PDBParser().get_structure(pdb_id, pdb_file)[0]
+    # Load model from original pdb file
+    model = pdb_parser.get_structure(pdb_id, pdb_file)[0]
+
+    # Fix PDB structure, use it later
+    pdb_file = fix_pdb(pdb_id, pdb_file, pdb_group)
 
     # Store annotation for entire complex
     pdb_annot = pdb_group.iloc[0]
@@ -42,8 +54,12 @@ for pdb_id, pdb_group in bypdb:
     antigen_seq = pdb_annot['antigen_seq']
     antigen_range = range(len(antigen_seq))
     antigen_residues = get_residues(antigen_chain, antigen_range)
+    antigen_seq_obs = get_seq(antigen_residues)
 
-    assert antigen_seq == get_seq(antigen_residues)
+    if antigen_seq != antigen_seq_obs:
+        warning("Antigen sequence mismatch (expected observed): ", antigen_seq, antigen_seq_obs,
+                ". Replacing with one from PDB.")
+        pdb_annot['antigen_seq'] = antigen_seq_obs
 
     # Iterate by TCR chain/region (CDR1,2,3)
     byregion = pdb_group.groupby(['chain_tcr', 'tcr_region'])
@@ -55,12 +71,30 @@ for pdb_id, pdb_group in bypdb:
         tcr_region_seq = tcr_annot['tcr_region_seq']
         tcr_region_range = range(tcr_annot['tcr_region_start'], tcr_annot['tcr_region_end'])
         tcr_region_residues = get_residues(tcr_chain, tcr_region_range)
+        tcr_region_seq_obs = get_seq(tcr_region_residues)
 
-        assert tcr_region_seq == get_seq(tcr_region_residues)
+        if tcr_region_seq != tcr_region_seq_obs:
+            warning("TCR:", tcr_region_id, " sequence mismatch (expected observed): ", tcr_region_seq,
+                    tcr_region_seq_obs, ". Replacing with one from PDB.")
+            tcr_annot['tcr_region_seq'] = tcr_region_seq_obs
 
         # Compute distances and add them to results
-        results.extend(calc_distances(tcr_annot['tcr_v_allele'], tcr_annot['tcr_region'],
+        results.extend(calc_distances(tcr_chain.get_id(), antigen_chain.get_id(),
+                                      tcr_annot['tcr_v_allele'], tcr_annot['tcr_region'],
                                       tcr_region_residues, antigen_residues, tcr_region_range, antigen_range))
 
-    print(results)
+        # Run the following separately for each region as GROMACS will not support >64 energy groups on NxN kernels
 
+        # Prepare GROMACS files - create index - run molecular dynamics
+        prepare_gmx(pdb_id, pdb_file, gmx_dir)
+
+        # Assign energy groups
+        keep_idxs = set()
+
+        for row in results:
+            keep_idxs.add(row['idx_tcr'])
+            keep_idxs.add(row['idx_ag'])
+
+        idxs = create_index(pdb_id, gmx_dir, [chain.get_id() for chain in model], keep_idxs)
+
+        run_md(pdb_id, gmx_dir, idxs)
--- structure/src/util.py
+++ structure/src/util.py
@@ -1,6 +1,16 @@
+from __future__ import print_function
+
 import itertools
 import os
 import sys
+from collections import OrderedDict
+from os.path import dirname
+from shutil import copyfile
+from subprocess import check_call
+
+from Bio.PDB import PDBParser
+from pdbfixer import PDBFixer
+from simtk.openmm.app import PDBFile
 
 
 def clear_folder(d):
@@ -16,15 +26,12 @@ def warning(*objs):
 
 # (Bio)PDB methods
 
-def get_required_chains(pdb_group):
-    return set(itertools.chain(*[pdb_group[c].tolist() for c in
-                                 ['chain_tcr', 'chain_antigen', 'chain_mhc_a', 'chain_mhc_b']]))
-
-
 _trans = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C',
           'GLU': 'E', 'GLN': 'Q', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
           'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P',
-          'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V'}
+          'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V',
+          # modified residues go below
+          'PFF': 'F', 'F2F': 'F'}
 
 
 def get_aa_code(res):
@@ -43,9 +50,12 @@ def calc_distance(aa1, aa2):
     return min([abs(atom1 - atom2) for atom1 in aa1 for atom2 in aa2])
 
 
-def calc_distances(tcr_v_allele, tcr_region, tcr_residues, ag_residues, tcr_range, ag_range):
+def calc_distances(tcr_chain, antigen_chain, tcr_v_allele, tcr_region, tcr_residues, ag_residues, tcr_range, ag_range):
+    # indexes are required to access gromacs data
     return [{'tcr_v_allele': tcr_v_allele,
              'tcr_region': tcr_region,
+             'idx_tcr': tcr_chain + '_' + str(aa_tcr.get_id()[1]),
+             'idx_ag': antigen_chain + '_' + str(aa_ag.get_id()[1]),
              'aa_tcr': get_aa_code(aa_tcr),
              'aa_ag': get_aa_code(aa_ag),
              'len_tcr': len(tcr_range),
@@ -55,3 +65,123 @@ def calc_distances(tcr_v_allele, tcr_region, tcr_residues, ag_residues, tcr_rang
              'dist': calc_distance(aa_tcr, aa_ag)}
             for i, aa_tcr in enumerate(tcr_residues)
             for j, aa_ag in enumerate(ag_residues)]
+
+
+# PDBfixer
+
+
+def get_required_chains(pdb_group):
+    return set(itertools.chain(*[pdb_group[c].tolist() for c in
+                                 ['chain_tcr', 'chain_antigen', 'chain_mhc_a', 'chain_mhc_b']]))
+
+
+def fix_pdb(pdb_id, pdb_file, pdb_group):
+    chains_to_retain = get_required_chains(pdb_group)
+    chains_to_remove = []
+
+    for chain in PDBParser().get_structure(pdb_id, pdb_file)[0]:
+        if chain.get_id() not in chains_to_retain:
+            chains_to_remove.append(chain.get_id())
+
+    fixer = PDBFixer(filename=pdb_file)
+
+    fixer.removeChains(chainIds=chains_to_remove)
+
+    fixer.findMissingResidues()
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
+    fixer.removeHeterogens(True)
+    fixer.addMissingHydrogens(7.0)
+    # fixer.addSolvent(fixer.topology.getUnitCellDimensions())
+
+    # KeepIds flag is critical here, otherwise we loose all information binding
+    pdb_file = dirname(pdb_file) + '/' + pdb_id + '.pdb'
+    PDBFile.writeFile(fixer.topology, fixer.positions, open(pdb_file, 'w'), keepIds=True)
+
+    return pdb_file
+
+
+# GROMACS
+
+os.environ['GMX_MAXBACKUP'] = '0'  # suppress shitload of gromacs backups
+
+
+def create_index(pdb_id, gmx_dir, chains, keep_idxs):
+    chains_iter = iter(chains)
+    i = 0
+    index = OrderedDict()
+
+    with open(gmx_dir + '/' + pdb_id + '.gro') as g_file:
+        for _ in range(2):
+            next(g_file)
+
+        prev_res_id = -1
+        chain = next(chains_iter)
+
+        for line in g_file:
+            if len(line) >= 44:
+                line = line.replace('\t', '    ')
+                res_id = int(line[0:5].strip())
+                atom_id = int(line[15:20].strip())
+
+                if res_id < prev_res_id:
+                    i += 1
+                    if i >= len(chains):
+                        break
+                    chain = next(chains_iter)
+
+                idx = chain + "_" + str(res_id)
+
+                if idx in keep_idxs:
+                    index.setdefault(idx, []).append(atom_id)
+
+                prev_res_id = res_id
+
+    res_ids = []
+
+    with open(gmx_dir + '/' + pdb_id + '.ndx', 'w') as i_file:
+        with open(gmx_dir + '/' + pdb_id + '.dat', 'w') as d_file:
+            d_file.write(str(len(index)) + '\n')
+            for res_id, atom_ids in index.items():
+                res_ids.append(res_id)
+                i_file.write('[ ' + res_id + ' ]\n')
+                d_file.write(res_id + '\n')
+                for atom_id in atom_ids:
+                    i_file.write(str(atom_id) + '\n')
+
+    return res_ids
+
+
+def prepare_gmx(pdb_id, pdb_file, gmx_dir):
+    os.chdir(gmx_dir)
+
+    check_call('gmx pdb2gmx -f {0} -o {1}.gro -p {1}.top -water spce -ff oplsaa; '
+               'gmx editconf -f {1}.gro -o {1}.gro -c -d 1.0 -bt cubic; '.
+               format(pdb_file, pdb_id), shell=True
+               )
+
+
+def run_md(pdb_id, gmx_dir, keep_idxs):
+    param_template_path = os.path.abspath("../res/sp.mdp")  # TODO: abs path from script
+
+    os.chdir(gmx_dir)
+
+    copyfile(param_template_path, pdb_id + '.mdp')
+
+    with open(pdb_id + '.mdp', 'a') as f:
+        f.write('\nenergygrps\t= ' + ' '.join(keep_idxs))
+
+    # Writing the following lines take several days owing to best-practices appliend in writing GROMACS documentation
+    # and the intuitiveness and diversity of GROMACS mailing lists Funny quotes written by GROMACS to stderr and
+    # mailing list replies containing solely (broken) documentation links to pages containing 5-10 words were really
+    # helpful during this process.
+
+    # Single-point energies are calculated with -rerun
+    # ndx/groups files are used to mark residues of interest and calculate interaction energies
+    # NOTE: without -nb CPU energies are not stored.
+
+    check_call('gmx grompp -c {}.gro -p {}.top -n {}.ndx -f {}.mdp -o {}.tpr; '
+               'gmx mdrun -s {}.tpr -rerun {}.gro -e {}.edr -g {}.log -nb cpu; '
+               'gmx enemat -groups {}.dat -nlevels 10000 -f {}.edr -emat .{}.xpm'
+               .format(pdb_id), shell=True
+               )
