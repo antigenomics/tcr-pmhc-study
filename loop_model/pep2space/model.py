335013c2f80d18cb994776ae0a23c2dc743ef0b4
Vadim Nazarov <vd.naz@yandex.ru>
2017-03-29 22:13:44 +0300

    more models; add LR regulation

--- loop_model/pep2space/model.py
+++ loop_model/pep2space/model.py
@@ -5,6 +5,7 @@ from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, BatchNormalization, concatenate, Input
 from keras.layers.advanced_activations import PReLU
 from keras.optimizers import Nadam
+from keras.callbacks import ReduceLROnPlateau
 
 from sklearn.cluster import MiniBatchKMeans
 
@@ -69,8 +70,8 @@ def train_model(max_pos, n_clust, coord, layers,
                 n_epochs, 
                 hist, model_list, 
                 how = "no", scale = "no", 
-                coord_fun = "onehot", model_fun = "dense"):
-    model_name = "left" + str(left_window) + "_right" + str(right_window) + "." + "-".join(map(str, layers)) + "." + how + "_" + scale + "." + coord_fun
+                features = "onehot", model_type = "dense"):
+    model_name = model_type + ".l" + str(left_window) + "_r" + str(right_window) + "." + "-".join(map(str, layers)) + "." + how + "_" + scale + "." + features
 
     if n_clust > 0:
         model_name += ".clust_" + str(n_clust)
@@ -84,43 +85,50 @@ def train_model(max_pos, n_clust, coord, layers,
             scale_data(df_cdr, how, scale)
             scale_data(df_can, how, scale)
 
-        if coord_fun == "onehot":
+        if features == "onehot":
             coord_fun = onehot
-        elif coord_fun == "omega":
+        elif features == "omega":
             coord_fun = onehot_omega
         else:
-            print("Unknown parameter", coord_fun)
+            print("Unknown parameter", features)
             return 0
         
         X_can, y_can = coord_fun(df_can, left_window, right_window, max_pos)
         X_cdr, y_cdr = coord_fun(df_cdr, left_window, right_window, max_pos)        
 
-        if model_fun == "dense":
+        if model_type == "dense":
             model_fun = dense_model
-        elif model_fun == "dense_pos":
+        elif model_type == "dense_pos":
             model_fun = dense_pos_model
             # add positions
-            X_can = [X_can, np.array([((x % 12) + 1) / 12. for x in range(X_can.shape[0])])]
-            X_cdr = [X_cdr, np.array([((x % 12) + 1) / 12. for x in range(X_cdr.shape[0])])]
+            X_can = [X_can, np.array([float((x % max_pos) + 1) / max_pos for x in range(X_can.shape[0])])]
+            X_cdr = [X_cdr, np.array([float((x % max_pos) + 1) / max_pos for x in range(X_cdr.shape[0])])]
         else:
             print("Unknown parameter", coord_fun)
             return 0
         
         model = model_fun((20*(right_window+left_window+1),), 1, layers)
 
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, cooldown=0, min_lr=0.0005)
+        
         if n_clust == 0:
-            hist_obj = model.fit(X_can, y_can, batch_size=64, epochs=n_epochs, verbose=0, validation_data=(X_cdr, y_cdr))
+            hist_obj = model.fit(X_can, y_can, batch_size=64, epochs=n_epochs, verbose=0, validation_data=(X_cdr, y_cdr), callbacks=[reduce_lr])
         else:
             kmeans = MiniBatchKMeans(n_clust, batch_size=1000, n_init=10)
-            kmeans.fit(X_can)
             
-            labels = kmeans.predict(X_can)
+            if model_type == "dense_pos":
+                kmeans.fit(X_can[0])
+                labels = kmeans.predict(X_can[0])
+            else:
+                kmeans.fit(X_can)
+                labels = kmeans.predict(X_can)
+                
             labels_cnt = Counter(labels)
             min_cluster, min_cluster_size = min(labels_cnt.items(), key = lambda x: x[1])
             
             weight_vec = np.array([np.log(min_cluster_size) / np.log(labels_cnt[x]) for x in labels])
             
-            hist_obj = model.fit(X_can, y_can, sample_weight=weight_vec, batch_size=64, epochs=n_epochs, verbose=0, validation_data=(X_cdr, y_cdr))
+            hist_obj = model.fit(X_can, y_can, sample_weight=weight_vec, batch_size=64, epochs=n_epochs, verbose=0, validation_data=(X_cdr, y_cdr), callbacks=[reduce_lr])
         
         hist[model_name] = hist_obj
         model_list[model_name] = model
