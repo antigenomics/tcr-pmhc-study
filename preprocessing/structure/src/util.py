28227a5fb9c578c2845801a6bd2a236ecbf1cd1c
Mikhail Shugay <mikhail.shugay@gmail.com>
2017-04-23 01:15:13 +0200

    Add tools for CDR superimposition

--- preprocessing/structure/src/util.py
+++ preprocessing/structure/src/util.py
@@ -1,18 +1,22 @@
 from __future__ import print_function
 
+import pandas as pd
 import itertools
 import os
 import sys
 import re
+import numpy as np
+from numpy.linalg import inv
 import math
 
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 from os.path import dirname
 from shutil import copyfile
 from subprocess import check_call
 from Bio.PDB import PDBParser, Vector
 from pdbfixer import PDBFixer
 from simtk.openmm.app import PDBFile
+from simtk.unit import is_quantity, angstroms, norm
 from Bio.PDB.Vector import rotaxis, rotmat
 
 
@@ -43,12 +47,59 @@ def get_aa_code(res):
     return _trans[res.get_resname()]
 
 
+def is_hydrogen(atom):
+    return atom.get_name().startswith("H")
+
+
 def get_seq(residues):
     return "".join(get_aa_code(res) for res in residues)
 
 
-def get_residues(residues, range):
-    return [x for i, x in enumerate(residues) if i in range]
+def get_residues(residues, query_range):
+    return [x for i, x in enumerate(residues) if i in query_range]
+
+
+def unpack_residues(residues):
+    return pd.DataFrame(list(itertools.chain(*[[(i, res.get_resname(), j, atom.get_name(), atom.get_vector()) for
+             j, atom in enumerate(res)] for i, res in enumerate(residues)])))
+
+
+def get_residues_pdbfixer(residues, query_range, original_residues):
+    # If we match everything is OK
+    exact = get_residues(residues, query_range)
+    if exact == get_residues(original_residues, query_range):
+        return exact
+
+    # Find range in original chain data
+    original_residue_list = get_residues(original_residues, query_range)
+
+    first = original_residue_list[0]
+    best_dist_first = 1000
+    best_first = None
+    last = original_residue_list[-1]
+    best_dist_last = 1000
+    best_last = None
+
+    # Find residues in new (fixed) chain data that have nearest CA coordinates
+    for i, x in enumerate(residues):
+        dist_first = calc_distance(first, x, 'CA')
+        if dist_first < best_dist_first:
+            best_dist_first = dist_first
+            best_first = (i, x)
+        dist_last = calc_distance(last, x, 'CA')
+        if dist_last < best_dist_last:
+            best_dist_last = dist_last
+            best_last = (i, x)
+
+    if best_dist_first > 0.1:
+        warning("Failed to find CA match between", first,
+                "and residues from fixed PDB, best match is", best_first)
+    if best_dist_last > 0.1:
+        warning("Failed to find CA match between", last,
+                "and residues from fixed PDB, best match is", best_last)
+
+    new_range = range(best_first[0], best_last[0] + 1)
+    return get_residues(residues, new_range)
 
 
 def get_calpha_pos(aa):
@@ -58,119 +109,22 @@ def get_calpha_pos(aa):
         return Vector(float('nan'), float('nan'), float('nan'))
 
 
-def rotate(residues):
-    start = get_calpha_pos(residues[0])
-    end = get_calpha_pos(residues[-1])
-
-    # C terminus -> N terminus axis
-    direction = end - start
-
-    # Matrix that rotates 'direction' to align with X axis
-    r = rotmat(direction, Vector(1, 0, 0))
-    
-    res = [(i, aa, (get_calpha_pos(aa) - start).left_multiply(r), (get_cbeta_pos(aa) - start).left_multiply(r))
-        for i, aa in enumerate(residues)]
-
-    # Find 'center of mass' coord in YZ plane
-    cm = Vector(0, 0, 0)
-    
-    for _, _, vec, _ in res:
-        cm = cm + Vector(0, vec[1], vec[2])
-    
-    # Matrix that rotates 'center of mass' to align with Z axis in YZ plane
-    r = rotmat(cm, Vector(0, 0, 1))
-
-    res = [(i, aa, vec[0], Vector(0, vec[1], vec[2]).left_multiply(r), vec_cb[0], Vector(0, vec_cb[1], vec_cb[2]).left_multiply(r)) 
-        for i, aa, vec, vec_cb in res]
-
-    return [(i, len(res), aa, Vector(x, vecYZ[1], vecYZ[2]), Vector(x_cb, vecYZ_cb[1], vecYZ_cb[2])) 
-        for i, aa, x, vecYZ, x_cb, vecYZ_cb in res]
-
-
-def calc_coords(tcr_chain, tcr_region, tcr_residues):
-    return [{'tcr_chain': tcr_chain,
-             'tcr_region': tcr_region,
-             'aa_tcr': get_aa_code(aa_tcr),
-             'len_tcr': l,
-             'pos_tcr': i,
-             'x': vec[0],
-             'y': vec[1],
-             'z': vec[2],
-             'x_cb': vec_cb[0],
-             'y_cb': vec_cb[1],
-             'z_cb': vec_cb[2]
-             }
-            for i, l, aa_tcr, vec, vec_cb in rotate(tcr_residues)]
-
-
-def calc_coords_ag(pdb_id, mhc_type, ag_residues):
-    return [{'pdb_id': pdb_id,
-             'mhc_type': mhc_type,
-             'aa_ag': get_aa_code(aa_ag),
-             'len_ag': l,
-             'pos_ag': i,
-             'x': vec[0],
-             'y': vec[1],
-             'z': vec[2],
-             'x_cb': vec_cb[0],
-             'y_cb': vec_cb[1],
-             'z_cb': vec_cb[2]
-             }
-            for i, l, aa_ag, vec, vec_cb in rotate(ag_residues)]
-
-
-def get_expected_cb_pos(aa):
-    n = aa['N'].get_vector() 
-    c = aa['C'].get_vector() 
-    ca = get_calpha_pos(aa)
-    # center at origin
-    n = n - ca
-    c = c - ca
-    # find rotation matrix that rotates n -120 degrees along the ca-c vector
-    rot = rotaxis(-math.pi*120.0/180.0, c)
-    # apply rotation to ca-n vector
-    cb_at_origin = n.left_multiply(rot)
-    # put on top of ca atom
-    return (cb_at_origin + ca)
-
-
-def get_cbeta_pos(aa):
-    if aa.get_resname() == 'GLY':
-        return get_expected_cb_pos(aa)
-    else:
-        ca = get_calpha_pos(aa)
-        try:
-            cb = aa['CB'].get_vector()
-        except KeyError:
-            cb = get_expected_cb_pos(aa)
-        return (ca + (((cb - ca).normalized()) ** 2.4))
+# Distance & energy
 
 
 def calc_distance(aa1, aa2, dist_type = 'closest_atom'):
-    if dist_type == 'CA':    
+    if dist_type == 'CA':
         return (get_calpha_pos(aa1) - get_calpha_pos(aa2)).norm()
-    elif dist_type == 'CB':    
-        return (get_cbeta_pos(aa1) - get_cbeta_pos(aa2)).norm()
-    else: # min dist)
-        return min([abs(atom1 - atom2) for atom1 in aa1 for atom2 in aa2])
+    else: # min dist
+        return min([abs(atom1 - atom2) for atom1 in aa1 if not is_hydrogen(atom1)
+            for atom2 in aa2 if not is_hydrogen(atom2)])
 
 
-def calc_bonds(aa1, aa2, dist_threshold = 4.5):
-    bonds = 0
-    for atom1 in aa1:
-        for atom2 in aa2:
-            if abs(atom1 - atom2) <= dist_threshold:
-                bonds = bonds + 1
-    return bonds
-
-
-def calc_distances(tcr_chain, antigen_chain, tcr_gene, tcr_v_allele, tcr_region, tcr_residues, ag_residues, tcr_range, ag_range):
-    # indexes are required to access GROMACS data
-    return [{'tcr_gene': tcr_gene,
+def calc_distances(tcr_v_allele, tcr_region, tcr_residues, ag_residues,
+                   tcr_range, ag_range):
+    return [{'tcr_gene': tcr_v_allele[0:3],
              'tcr_v_allele': tcr_v_allele,
              'tcr_region': tcr_region,
-             'idx_tcr': tcr_chain + '_' + str(aa_tcr.get_id()[1]),
-             'idx_antigen': antigen_chain + '_' + str(aa_ag.get_id()[1]),
              'aa_tcr': get_aa_code(aa_tcr),
              'aa_antigen': get_aa_code(aa_ag),
              'len_tcr': len(tcr_range),
@@ -178,10 +132,84 @@ def calc_distances(tcr_chain, antigen_chain, tcr_gene, tcr_v_allele, tcr_region,
              'pos_tcr': i,
              'pos_antigen': j,
              'distance': calc_distance(aa_tcr, aa_ag),
-             'distance_CA': calc_distance(aa_tcr, aa_ag, 'CA'),
-             'nbonds': calc_bonds(aa_tcr, aa_ag)}
+             'distance_CA': calc_distance(aa_tcr, aa_ag, 'CA')}
             for i, aa_tcr in enumerate(tcr_residues)
             for j, aa_ag in enumerate(ag_residues) if aa_ag.get_resname() not in _skip]
+
+
+# Rotations and superimpositions
+
+CanonicalTransformation = namedtuple('CanonicalTransformation',
+                                     'offset Rx Rz')
+
+
+def find_canonical_transform(residues):
+    start = get_calpha_pos(residues[0])
+    end = get_calpha_pos(residues[-1])
+
+    # C terminus -> N terminus axis
+    direction = end - start
+
+    # Matrix that rotates 'direction' to align with X axis
+    # i.e. rotation along Z axis
+    Rz = rotmat(direction, Vector(1, 0, 0))
+
+    res = [(i, aa, (get_calpha_pos(aa) - start).left_multiply(Rz))
+        for i, aa in enumerate(residues)]
+
+    # Find 'center of mass' coord in YZ plane
+    cm = Vector(0, 0, 0)
+
+    for _, _, vec in res:
+        cm = cm + Vector(0, vec[1], vec[2])
+
+    # Matrix that rotates 'center of mass' to align with Z axis in YZ plane
+    # i.e. rotation along X axis
+    Rx = rotmat(cm, Vector(0, 0, 1))
+
+    Rx[0,:] = np.array([1.0, 0.0, 0.0])
+    Rx[:,0] = np.array([1.0, 0.0, 0.0])
+
+    return CanonicalTransformation(offset = start,
+                                   Rz = Rz, Rx = Rx)
+
+
+def move_residue(res, sfun):
+    res1 = res.copy()
+
+    for atom in res1:
+        atom.set_coord(sfun(atom.get_vector()).get_array())
+
+    return res1
+
+
+def to_canonical(residues, ct):
+    ctfun = lambda x: (x - ct.offset).left_multiply(ct.Rz).left_multiply(ct.Rx)
+
+    return [move_residue(res, ctfun) for res in residues]
+
+
+def from_canonical(residues, ct):
+    Rxi = inv(ct.Rx)
+    Rzi = inv(ct.Rz)
+    ctifun = lambda x: ct.offset + x.left_multiply(Rxi).left_multiply(Rzi)
+
+    return [move_residue(res, ctifun) for res in residues]
+
+
+def superimpose(residues_ref, residues_query, tcr_region):
+    # Find canonical transformation for query and apply it
+    ctq = find_canonical_transform(residues_query)
+    residues_query = to_canonical(residues_query, ctq)
+
+    # Find canonical transformation for reference
+    ctr = find_canonical_transform(residues_ref)
+
+    # Move canonicalized query atoms to reference
+    residues_query = from_canonical(residues_query, ctr)
+
+    return residues_query
+
 
 
 # PDBfixer
@@ -205,133 +233,105 @@ def fix_pdb(pdb_id, pdb_file, pdb_group):
     fixer.removeChains(chainIds=chains_to_remove)
 
     fixer.findMissingResidues()
-    fixer.findNonstandardResidues()
-    fixer.replaceNonstandardResidues()
     fixer.findMissingAtoms()
     fixer.addMissingAtoms()
     fixer.removeHeterogens(True)
-    #fixer.addMissingHydrogens(7.0)
+    fixer.addMissingHydrogens(7.4)
 
     # KeepIds flag is critical here, otherwise we loose all information binding
     pdb_file = dirname(pdb_file) + '/' + pdb_id + '.pdb'
-    PDBFile.writeFile(fixer.topology, fixer.positions, open(pdb_file, 'w'), keepIds=True)
+
+    f = open(pdb_file, 'w')
+
+    PDBFile.writeHeader(fixer.topology, f)
+    PDBFile_writeModel(fixer.topology, fixer.positions, f)
+    PDBFile.writeFooter(fixer.topology, f)
 
     return pdb_file
 
+# Below goes modified OPENMM code
+# from https://github.com/pandegroup/openmm/blob/f21bb6a5434eafe1fbf315cf686fd2d198ddbde0/wrappers/python/simtk/openmm/app/pdbfile.py
+# Preserving chain IDs but renumberig residues, this solves https://github.com/pandegroup/pdbfixer/issues/153 for us
 
-# GROMACS
+def PDBFile_writeModel(topology, positions, file=sys.stdout, modelIndex=None):
+    """Write out a model to a PDB file.
+    Parameters
+    ----------
+    topology : Topology
+        The Topology defining the model to write
+    positions : list
+        The list of atomic positions to write
+    file : file=stdout
+        A file to write the model to
+    modelIndex : int=None
+        If not None, the model will be surrounded by MODEL/ENDMDL records
+        with this index
+    """
 
-os.environ['GMX_MAXBACKUP'] = '0'  # suppress gromacs backups
+    if len(list(topology.atoms())) != len(positions):
+        raise ValueError('The number of positions must match the number of atoms')
+    if is_quantity(positions):
+        positions = positions.value_in_unit(angstroms)
+    if any(math.isnan(norm(pos)) for pos in positions):
+        raise ValueError('Particle position is NaN')
+    if any(math.isinf(norm(pos)) for pos in positions):
+        raise ValueError('Particle position is infinite')
+    nonHeterogens = PDBFile._standardResidues[:]
+    nonHeterogens.remove('HOH')
+    atomIndex = 1
+    posIndex = 0
+    if modelIndex is not None:
+        print("MODEL     %4d" % modelIndex, file=file)
+    for (chainIndex, chain) in enumerate(topology.chains()):
+        chainName = chain.id
 
+        residues = list(chain.residues())
+        for (resIndex, res) in enumerate(residues):
+            if len(res.name) > 3:
+                resName = res.name[:3]
+            else:
+                resName = res.name
 
-def create_index(pdb_id, pdb_sub_id, keep_idxs):
-    # TODO: check AAs
+            resId = "%4d" % ((resIndex+1)%10000)
 
-    index = OrderedDict()
-
-    with open(pdb_id + '.pdb') as g_file:
-        for _ in range(4):
-            next(g_file)
-
-        for line in g_file:
-            if line[0:3] == 'TER':
-                continue
-            if line[0:6] == 'ENDMDL':
-                break
-
-            res_id = int(line[22:26].strip())
-            atom_id = int(line[4:11].strip())
-            chain_id = line[20:22].strip()
-
-            idx = chain_id + "_" + str(res_id)
-
-            if idx in keep_idxs:
-                index.setdefault(idx, []).append(atom_id)
-
-    res_ids = []
-
-    with open(pdb_sub_id + '.ndx', 'w') as i_file:
-        with open(pdb_sub_id + '.dat', 'w') as d_file:
-            d_file.write(str(len(index)) + '\n')
-            for res_id, atom_ids in index.items():
-                res_ids.append(res_id)
-                i_file.write('[ ' + res_id + ' ]\n')
-                d_file.write(res_id + '\n')
-                for atom_id in atom_ids:
-                    i_file.write(str(atom_id) + '\n')
-
-    missing_res = [x for x in keep_idxs if x not in res_ids]
-    if missing_res:
-        warning("The following residues are missing in processed structure: ",
-                "; ".join(missing_res))
-
-    return res_ids
-
-
-def prepare_gmx(pdb_id, pdb_file, gmx_dir, param_template_path):
-    param_template_path += "/ions.mdp"
-
-    check_call('gmx pdb2gmx -f {0} -o {1}.pdb -p {1}.top -water tip3p -ff oplsaa -ignh >> gmx.log 2>&1; '
-               'gmx editconf -f {1}.pdb -o {1}.pdb -c -d 1.2 -bt cubic >> gmx.log 2>&1; '
-               'gmx solvate -cp {1}.pdb -cs spc216.gro -o {1}.pdb -p {1}.top >> gmx.log 2>&1; '.
-               # 'gmx grompp -f {2} -c {1}.pdb -p {1}.top -o {1}.ions.tpr >> gmx.log 2>&1; '
-               # 'gmx genion -nice 0 -neutral -s {1}.ions.tpr -o {1}.pdb -p {1}.top '
-               # '-pname NA -nname CL -conc 0.154 -rmin 0.4 >> gmx.log 2>&1'.
-               format(pdb_file, pdb_id, param_template_path), shell=True
-               )
-
-
-def run_minimization(pdb_id, param_template_path):
-    param_template_path += '/minim.mdp'
-
-    check_call('gmx grompp -c {0}.pdb -p {0}.top -f {1} -o {0}.m.tpr >> gmx.log 2>&1; '
-               'gmx mdrun -s {0}.m.tpr -c {0}.m.pdb -nb cpu >> gmx.log 2>&1; '
-               .format(pdb_id, param_template_path), shell=True
-               )
-
-
-def run_single_point(pdb_id, pdb_sub_id, param_template_path, minimized, keep_idxs):
-    copyfile(param_template_path + '/sp.mdp', pdb_sub_id + '.sp.mdp')
-
-    with open(pdb_sub_id + '.sp.mdp', 'a') as f:
-        f.write('\nenergygrps\t= ' + ' '.join(keep_idxs))
-
-    suffix = '.m' if minimized else ''
-
-    # Single-point energies are calculated with -rerun
-    # ndx/groups files are used to mark residues of interest and calculate interaction energies
-    # NOTE: without -nb CPU energies are not stored.
-
-    check_call('gmx grompp -c {1}{2}.pdb -p {1}.top -n {0}.ndx -f {0}.sp.mdp -o {0}.tpr >> gmx.log 2>&1; '
-               'gmx mdrun -s {0}.tpr -rerun {1}{2}.pdb -e {0}.edr -nb cpu >> gmx.log 2>&1; '
-               'gmx enemat -groups {0}.dat -nlevels 10000 -f {0}.edr -emat .{0}.xpm >> gmx.log 2>&1'
-               .format(pdb_sub_id, pdb_id, suffix), shell=True
-               )
+            if res.name in nonHeterogens:
+                recordName = "ATOM  "
+            else:
+                recordName = "HETATM"
+            for atom in res.atoms():
+                if atom.element is not None:
+                    symbol = atom.element.symbol
+                else:
+                    symbol = ' '
+                if len(atom.name) < 4 and atom.name[:1].isalpha() and len(symbol) < 2:
+                    atomName = ' '+atom.name
+                elif len(atom.name) > 4:
+                    atomName = atom.name[:4]
+                else:
+                    atomName = atom.name
+                coords = positions[posIndex]
+                line = "%s%5d %-4s %3s %s%4s    %s%s%s  1.00  0.00          %2s  " % (
+                    recordName, atomIndex%100000, atomName, resName, chainName, resId, _format_83(coords[0]),
+                    _format_83(coords[1]), _format_83(coords[2]), symbol)
+                assert len(line) == 80, 'Fixed width overflow detected'
+                print(line, file=file)
+                posIndex += 1
+                atomIndex += 1
+            if resIndex == len(residues)-1:
+                print("TER   %5d      %3s %s%4s" % (atomIndex, resName, chainName, resId), file=file)
+                atomIndex += 1
+    if modelIndex is not None:
+        print("ENDMDL", file=file)
 
 
-def read_enemat(pdb_sub_id, ene_term, idxs):
-    with open(ene_term + '.' + pdb_sub_id + '.xpm') as x_file:
-        line = ""
-        while "static char *gromacs_xpm[] = {" not in line:
-            line = next(x_file)
-
-        next(x_file)
-
-        levels = {}
-        while "x-axis" not in line:
-            line = next(x_file)
-            tokens = re.split('[ "]', line)
-            levels[tokens[1]] = float(tokens[8])
-
-        next(x_file)
-
-        word_size = len(list(levels.keys())[0])
-
-        results = {}
-        i = 0
-        for line in x_file:
-            for j in range(len(idxs)):
-                results[(idxs[-i - 1], idxs[j])] = levels[line[(1 + word_size * j):(1 + word_size * (j + 1))]]
-            i += 1
-
-    return results
+def _format_83(f):
+    """Format a single float into a string of width 8, with ideally 3 decimal
+    places of precision. If the number is a little too large, we can
+    gracefully degrade the precision by lopping off some of the decimal
+    places. If it's much too large, we throw a ValueError"""
+    if -999.999 < f < 9999.999:
+        return '%8.3f' % f
+    if -9999999 < f < 99999999:
+        return ('%8.3f' % f)[:8]
+    raise ValueError('coordinate "%s" could not be represented '
+                     'in a width-8 field' % f)
